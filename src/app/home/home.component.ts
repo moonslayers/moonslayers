b031a9b371057c345bdf5ce40da0c891a959911a
Jorge <jorge.marquez.luna@uabc.edu.mx>
2024-04-16 21:50:29 -0700

    ss

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  jorge:string=''
+  slide_animation:string='animate__bounceInUp'
+  tab:number=0
+  animation:boolean=false
 
-  animation_jorge(){
-    console.log("Mouse dentro")
-    this.jorge='animate_animated animate__lightSpeedInRight'
-    setTimeout(()=>{
-      this.jorge=''
-    },1000)
+  private lastScrollTop = 0;
+
+  down(){
+    console.log(event)
+    if(!this.animation){
+      this.animation=true
+      this.slide_animation='animate__bounceOutUp'
+      setTimeout(()=>{
+        this.tab++
+        if(this.tab>2){
+          this.tab=0
+        }
+        this.animation=false
+        this.slide_animation='animate__bounceInUp'
+      },800)
+    }
+  }
+
+  up(){
+    if(!this.animation){
+      this.animation=true
+      this.slide_animation='animate__bounceOutDown'
+      setTimeout(()=>{
+        this.tab--
+        if(this.tab<0){
+          this.tab=2
+        }
+        this.animation=false
+        this.slide_animation='animate__bounceInDown'
+      },800)
+    }
   }
 }
