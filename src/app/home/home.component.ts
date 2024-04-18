097687caae2af1f6a72232a036b6f05a445debac
Jorge <jorge.marquez.luna@uabc.edu.mx>
2024-04-17 22:03:48 -0700

    mandatory behavior

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, HostListener } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -11,40 +11,92 @@ import { Component, HostListener } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  slide_animation:string='animate__bounceInUp'
-  tab:number=0
-  animation:boolean=false
+  @ViewChild('div1') miElemento1: ElementRef | undefined;
+  @ViewChild('div2') miElemento2: ElementRef | undefined;
+  @ViewChild('div3') miElemento3: ElementRef | undefined;
+  slide_animation: string = 'animate__bounceInUp'
+  tab: number = 0
+  animation: boolean = false
+  elements: any[] = [
+    {
+      isVisible: false,
+    },
+    {
+      isVisible: false
+    },
+    {
+      isVisible: false
+    },
+  ]
 
-  private lastScrollTop = 0;
-
-  down(){
-    console.log(event)
-    if(!this.animation){
-      this.animation=true
-      this.slide_animation='animate__bounceOutUp'
-      setTimeout(()=>{
-        this.tab++
-        if(this.tab>2){
-          this.tab=0
-        }
-        this.animation=false
-        this.slide_animation='animate__bounceInUp'
-      },800)
+  ngAfterViewInit() {
+    // Aquí puedes acceder al elemento después de que se hayan inicializado las vistas
+    if (typeof IntersectionObserver !== 'undefined') {
+      this.verificarVisibilidad();
+    } else {
+      console.warn('IntersectionObserver no es compatible con este navegador.');
     }
   }
 
-  up(){
-    if(!this.animation){
-      this.animation=true
-      this.slide_animation='animate__bounceOutDown'
-      setTimeout(()=>{
-        this.tab--
-        if(this.tab<0){
-          this.tab=2
-        }
-        this.animation=false
-        this.slide_animation='animate__bounceInDown'
-      },800)
+  verificarVisibilidad() {
+    const options = {
+      root: null, // Use el viewport como área de observación
+      rootMargin: '0px', // Sin margen adicional
+      threshold: 0.5 // Umbral de visibilidad: 0.5 significa que se activará cuando al menos el 50% del elemento sea visible
+    };
+
+
+    if (this.elements[0]) {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            this.elements[0].isVisible = true
+            setTimeout(() => {
+              this.elements[0].isVisible = false
+            }, 800)
+          } else {
+          }
+        });
+      }, options);
+
+      if (this.miElemento1 && observer) {
+        console.log(this.miElemento1)
+        observer.observe(this.miElemento1.nativeElement);
+      }
+    }
+    if (this.elements[1]) {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            this.elements[1].isVisible = true
+            setTimeout(() => {
+              this.elements[1].isVisible = false
+            }, 800)
+          } else {
+          }
+        });
+      }, options);
+
+      if (this.miElemento2) {
+        observer.observe(this.miElemento2.nativeElement);
+      }
+    }
+    if (this.elements[2]) {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            this.elements[2].isVisible = true
+            setTimeout(() => {
+              this.elements[2].isVisible = false
+            }, 800)
+          } else {
+          }
+        });
+      }, options);
+
+      if (this.miElemento3 && observer) {
+        observer.observe(this.miElemento3.nativeElement);
+      }
     }
   }
 }
